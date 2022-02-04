d23ee70d814bf60694f40706b5d811cdb8cfe85f
Massakera <enzomassaki@hotmail.com>
2022-02-04 07:47:45 -0300

    building a padding function

--- main.ts
+++ main.ts
@@ -4,3 +4,20 @@ function getFinalPrice(price: number, discount: number) {
 
 console.log(getFinalPrice(100,10));
 console.log(getFinalPrice(100,20));
+
+function calcTax(state: string, income: number, dependents: number): number {
+    if (state=='NY') {
+        return income * 0.06 - dependents * 500;
+    } else if (state === 'NJ') {
+        return income * 0.05 - dependents * 300;
+    }
+}
+
+function padLeft (value: string, padding: number | string): string {
+    if (typeof padding === "number") {
+        return Array(padding+1).join(" ") + value;
+    }
+    if (typeof padding === "string") {
+        return padding + value;
+    }
+}
